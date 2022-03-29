ff9e2533f2a3d54adac5e3554030142dda7b58db
Alan Poveda <alsepoveda@gmail.com>
2022-03-29 20:30:11 -0300

    The final resolution

--- lib/challengeelixir02.ex
+++ lib/challengeelixir02.ex
@@ -1,28 +1,50 @@
+
+
+# defmodule Challengeelixir02 do
+#   defp onlyInteger(list) do
+#     Enum.flat_map(list, fn string ->
+#       case Integer.parse(string) do
+#         # transform to integer
+#         {int, _rest} -> [int]
+#         # skip the value
+#         :error -> []
+#       end
+#     end)
+#   end
+
+#   defp oddNumber(list) do
+#     Enum.filter(list, fn num ->
+#       isodd(num)
+#     end)
+#   end
+
+#   defp isodd(value) do
+#     Integer.mod(value, 2) != 0
+#   end
+
+#   def call(list) do
+#     onlyInteger(list)
+#     |> oddNumber
+#     |> length
+#   end
+# end
+
+
+
 defmodule Challengeelixir02 do
-  defp onlyInteger(list) do
-    Enum.flat_map(list, fn string ->
-      case Integer.parse(string) do
-        # transform to integer
-        {int, _rest} -> [int]
-        # skip the value
-        :error -> []
-      end
-    end)
-  end
-
-  defp oddNumber(list) do
-    Enum.filter(list, fn num ->
-      isodd(num)
-    end)
-  end
-
-  defp isodd(value) do
-    Integer.mod(value, 2) != 0
-  end
-
+  require Integer
   def call(list) do
-    onlyInteger(list)
-    |> oddNumber
-    |> length
+    list
+    |> Enum.map(&to_integer/1)
+    |> Enum.filter(&is_integer/1)
+    |> Enum.filter(&Integer.is_odd/1)
+    |> Enum.count()
+  end
+
+  defp to_integer(str) do
+    case Integer.parse(str) do
+      {int, ""} -> int
+      _ -> :naan
+    end
   end
 end
